dcc0bf7c24a11cabfb0ff1e5603b0ae4bedb4961
MasterMind <87894877+Master-Mind-007@users.noreply.github.com>
2022-07-22 17:59:25 +0530

    Update ss.php

--- modules/checker/ss.php
+++ modules/checker/ss.php
@@ -248,7 +248,6 @@ if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){
             
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $stripejs);
-            curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
             curl_setopt($ch, CURLOPT_HEADER, 0);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Host: m.stripe.com',
@@ -273,7 +272,6 @@ if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){
 
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $auth);
-            curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
             curl_setopt($ch, CURLOPT_HEADER, 0);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Host: m.stripe.com',
@@ -298,7 +296,6 @@ if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){
     
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $final);
-            curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
             curl_setopt($ch, CURLOPT_HEADER, 0);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Host: m.stripe.com',
