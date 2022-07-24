643382f6b6ca18c52faddbde527b7abdba8c30f1
MasterMind <87894877+Master-Mind-007@users.noreply.github.com>
2022-07-24 08:11:49 +0530

    Update main.php

--- main.php
+++ main.php
@@ -30,54 +30,44 @@ include __DIR__."/modules/checker/sm.php";
 //////////////===[START]===//////////////
 
 if(strpos($message, "/start") === 0){
-if(!isBanned($userId) && !isMuted($userId)){
+    if(!isBanned($userId) && !isMuted($userId)){
 
-  if($userId == $config['adminID']){
-    $messagesec = "<b>Type /admin to know admin commands</b>";
-  }
+        if($userId == $config['adminID']){
+            $messagesec = "<b>Type /admin to know admin commands</b>";
+        }
 
-    addUser($userId);
-    bot('sendmessage',[
-        'chat_id'=>$chat_id,
-        'text'=>"<b>Hello @$username,
-
+        addUser($userId);
+        bot('sendmessage',[
+            'chat_id'=>$chat_id,
+            'text'=>"<b>Hello @$username,
 Type /cmds to know all my commands!</b>
-
 $messagesec",
-	'parse_mode'=>'html',
-	'reply_to_message_id'=> $message_id,
-    'reply_markup'=>json_encode(['inline_keyboard' => [
-        [
-          ['text' => "💠 Created By 💠", 'url' => "t.me/Arceus69_Xd"]
-        ],
-        [
-          ['text' => "💎 Source Code 💎", 'url' => "t.me/ninjanaveen"]
-        ],
-      ], 'resize_keyboard' => true])
-        
-    ]);
-  }
+        'parse_mode'=>'html',
+        'reply_to_message_id'=> $message_id,
+
+        ]);
+    }
 }
 
 //////////////===[CMDS]===//////////////
 
 if(strpos($message, "/cmds") === 0 || strpos($message, "!cmds") === 0){
 
-  if(!isBanned($userId) && !isMuted($userId)){
-    bot('sendmessage',[
-    'chat_id'=>$chat_id,
-    'text'=>"<b>Which commands would you like to check?</b>",
-    'parse_mode'=>'html',
-    'reply_to_message_id'=> $message_id,
-    'reply_markup'=>json_encode(['inline_keyboard'=>[
-    [['text'=>"💳 CC Checker Gates",'callback_data'=>"checkergates"]],[['text'=>"🛠 Other Commands",'callback_data'=>"othercmds"]],
-    ],'resize_keyboard'=>true])
-    ]);
-  }
-  
-  }
-  
-  if($data == "back"){
+    if(!isBanned($userId) && !isMuted($userId)){
+        bot('sendmessage',[
+        'chat_id'=>$chat_id,
+        'text'=>"<b>Which commands would you like to check?</b>",
+        'parse_mode'=>'html',
+        'reply_to_message_id'=> $message_id,
+        'reply_markup'=>json_encode(['inline_keyboard'=>[
+        [['text'=>"💳 CC Checker Gates",'callback_data'=>"checkergates"]],[['text'=>"🛠 Other Commands",'callback_data'=>"othercmds"]],
+        ],'resize_keyboard'=>true])
+        ]);
+    }
+
+}
+
+if($data == "back"){
     bot('editMessageText',[
     'chat_id'=>$callbackchatid,
     'message_id'=>$callbackmessageid,
@@ -87,61 +77,43 @@ if(strpos($message, "/cmds") === 0 || strpos($message, "!cmds") === 0){
     [['text'=>"💳 CC Checker Gates",'callback_data'=>"checkergates"]],[['text'=>"🛠 Other Commands",'callback_data'=>"othercmds"]],
     ],'resize_keyboard'=>true])
     ]);
-  }
-  
-  if($data == "checkergates"){
+}
+
+if($data == "checkergates"){
     bot('editMessageText',[
     'chat_id'=>$callbackchatid,
     'message_id'=>$callbackmessageid,
     'text'=>"<b>━━CC Checker Gates━━</b>
-  
+
 <b>/ss | .ss - Stripe [Auth]</b>
-<b>/ss2 | .ss2 - Stripe - 2[Auth]</b>
-<b>/chk | .chk - Stripe [Merchant]</b>
-<b>/sch | .sch - User Stripe Merchant [Needs SK]</b>
-<b>/b3 | .b3 - Braintree Merchant [Auth]</b>
-<b>/br | .br - Braintree Merchant Gate-2[Auth]</b>
-<b>/chg | .chg - Stripe 20$ Chrg [Charge]</b>
-<b>/ch2 | .ch2 - Stripe 8.50$ Chrg [Charge]</b>
-<b>/shp | .shp - Shopify 8.50$ [Charge]</b>
-<b>/sh2 | .sh2 - Shopify 10$ [Charge]</b>
-<b>/pp | .pp - Paypal [Auth]</b>
-<b>/sch | .sch - User Stripe Merchant [Needs SK]</b>
-<b>/gf | .gf - For GirlFrnd [Needs Male Gender]</b>
-<b>/rnd | .rnd - For Randi [Needs Dick]</b>
-
 <b>/apikey sk_live_xxx - Add SK Key for /schk gate</b>
-<b>/myapikey | !myapikey - View the added SK Key for /schk gate</b>
-
-<b>ϟ Join <a href='t.me/Not_pro_noober'>Dazai Osamu</a></b>",
+<b>/myapikey | !myapikey - View the added SK Key for /schk gate</b>",
     'parse_mode'=>'html',
     'disable_web_page_preview'=>true,
     'reply_markup'=>json_encode(['inline_keyboard'=>[
   [['text'=>"Return",'callback_data'=>"back"]]
   ],'resize_keyboard'=>true])
   ]);
-  }
-  
-  
-  if($data == "othercmds"){
+}
+
+
+if($data == "othercmds"){
     bot('editMessageText',[
     'chat_id'=>$callbackchatid,
     'message_id'=>$callbackmessageid,
     'text'=>"<b>━━Other Commands━━</b>
-  
+
 <b>/me | !me</b> - Your Info
 <b>/stats | !stats</b> - Checker Stats
 <b>/key | !key</b> - SK Key Checker
 <b>/bin | !bin</b> - Bin Lookup
-<b>/iban | !iban</b> - IBAN Checker
-  
-  <b>ϟ Join <a href='t.me/IndianBots'>IndianBots</a></b>",
+<b>/iban | !iban</b> - IBAN Checker",
     'parse_mode'=>'html',
     'disable_web_page_preview'=>true,
     'reply_markup'=>json_encode(['inline_keyboard'=>[
   [['text'=>"Return",'callback_data'=>"back"]]
   ],'resize_keyboard'=>true])
   ]);
-  }
+}
 
 ?>
